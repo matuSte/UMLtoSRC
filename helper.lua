10cc6a8be27fd317894c8093a49245a7d1ff996f
matuSte <matuste1@gmail.com>
2016-10-17 15:22:28 +0200

    Add files via upload

---
+++ helper.lua
@@ -0,0 +1,54 @@
+local M = {}
+ 
+-- get textcontent of file
+function M.getFile(filename)
+  local f = assert(io.open(filename, "r"))
+  local text = f:read("*all")
+  f:close()
+  
+  return text
+end
+
+-- print content of table
+function M.printTable(myTable)
+  for k, v in pairs( myTable ) do
+     print(k, v)
+  end
+end
+
+-- print content of table recursive
+function M.printTable_r(t)
+  local print_r_cache={}
+    local function sub_print_r(t,indent)
+        if (print_r_cache[tostring(t)]) then
+            print(indent.."*"..tostring(t))
+        else
+            print_r_cache[tostring(t)]=true
+            if (type(t)=="table") then
+                for pos,val in pairs(t) do
+                    if (type(val)=="table") then
+                        print(indent.."["..pos.."] => "..tostring(t).." {")
+                        sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
+                        print(indent..string.rep(" ",string.len(pos)+6).."}")
+                    elseif (type(val)=="string") then
+                        print(indent.."["..pos..'] => "'..val..'"')
+                    else
+                        print(indent.."["..pos.."] => "..tostring(val))
+                    end
+                end
+            else
+                print(indent..tostring(t))
+            end
+        end
+    end
+    if (type(t)=="table") then
+        print(tostring(t).." {")
+        sub_print_r(t,"  ")
+        print("}")
+    else
+        sub_print_r(t,"  ")
+    end
+    print()
+end
+
+return M
