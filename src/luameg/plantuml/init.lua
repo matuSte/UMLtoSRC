e97f050cd196697b6c5935987963d88cb71ab649
Matus <matuste1@gmail.com>
2017-04-05 16:08:24 +0200

    [ADD] Added interface to plantuml submodule

---
+++ src/luameg/plantuml/init.lua
@@ -0,0 +1,11 @@
+--------------------------
+-- INIT luameg plantuml
+-- @release 2017/04/05 Matúš Štefánik
+--------------------------
+
+local classDiagram = require "luameg.plantuml.classDiagram"
+
+return {
+	getClassPlantuml = classDiagram.getPlantUmlFromNode,
+	getClassDiagramSvg = classDiagram.getImageFromNode
+}
