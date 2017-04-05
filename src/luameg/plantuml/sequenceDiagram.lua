80d15fa061dce3f6881482330eac9fb5ad162c89
thomas <cyhex17@gmail.com>
2017-04-05 22:42:49 +0200

    Implements image generator for sequence diagram with luadb graph and given method node ID.

--- src/luameg/plantuml/sequenceDiagram.lua
+++ src/luameg/plantuml/sequenceDiagram.lua
@@ -1,101 +1,108 @@
-function deactivationHelper(file, alreadyUsedClasses, activeMethodCalls, activeClass, actualMethodCall)
-
-  while not (activeClass == actualMethodCall.classCalledWithin) do
-  
-    local lastMethodCall = activeMethodCalls[1]
-    
-    file:write("deactivate " .. lastMethodCall.calledTo .. "\n")
-    alreadyUsedClasses[lastMethodCall.calledTo] = false
-    activeClass = lastMethodCall.calledFrom
-    table.remove(activeMethodCalls, 1)
-  
-  end
-
-  return file, alreadyUsedClasses, activeMethodCalls, activeClass
-end
-
-
-function generateSequenceDiagramTxt(methodCalls, startingClass, startingMethod)
-
-  local file = io.open("plantUml.txt", "w")
-  local alreadyUsedClasses = {}
-  local activeMethodCalls = {}
-  local activeClass = startingClass
-  
-  file:write("@startuml\n")
-  
-  file:write("participant User\n")
-  
-  file:write("User -> " .. startingClass .. " : " .. startingMethod .. "\n")
-  file:write("activate " .. startingClass .. "\n")
-  alreadyUsedClasses[startingClass] = true
-  
-  for key, value in pairs(methodCalls) do
-  
-    if (value.structure == "method") then
-    
---    we need to deactivate inactive classes
---      file, alreadyUsedClasses, activeMethodCalls, activeClass = deactivationHelper(file, alreadyUsedClasses, activeMethodCalls, activeClass, value)
-
-    
---      create new method call only if it is to another class
-      if not (value.classCalledTo == value.classCalledWithin) then
-        local newMethodCall = {
-          calledFrom = value.classCalledWithin,
-          calledTo = value.classCalledTo
-        }
-        table.insert(activeMethodCalls, 1, newMethodCall)
-        activeClass = value.classCalledTo
-      end
-    
-      file:write(value.classCalledWithin .. " -> " .. value.classCalledTo .. " : " .. value.name .. "\n")
---      if not (alreadyUsedClasses[value.classCalledTo]) then
-      file:write("activate " .. value.classCalledTo .. "\n")
---        alreadyUsedClasses[value.classCalledTo] = true
---      end
-      
-    elseif (value.structure == "condition-if") then
-    
-      file:write("alt " .. value.name .. "\n")
-    
-    elseif (value.structure == "condition-else") then
-    
-      file:write("else " .. value.name .. "\n")
-    
-    elseif (value.structure == "condition-end") or (value.structure == "loop-end") then
-    
-      file:write("end\n")
-      
-    elseif (value.structure == "loop") then
-    
-      file:write("loop " .. value.name .. "\n")
-    
-    elseif (value.structure == "return") then
-    
-      file:write(value.classCalledWithin .. " --> " .. value.classCalledTo .. "\n")
-      
-      alreadyUsedClasses[value.classCalledWithin] = false
-    
-    elseif (value.structure == "method-end") then
-    
-      file:write("deactivate " .. value.classCalledTo .. "\n")
-    
-    end
-  
-  end
-  
-  file:write("deactivate " .. startingClass .. "\n")
-  
-  file:write("@enduml")
-
-  file:close()
-end
 
 function generateSequenceDiagramImage()
   os.execute("java -jar plantuml.jar -verbose plantUml.txt")
 end
 
+-----------------------------------------------------
+
+-- Returns part of luadb graph representing sequence diagram for given method - format = string
+-- to save into file and then generate svg with use of plantUML
+local function filterSequenceDiagram( graph, methodNodeID, outputStr, classCalledFrom )
+
+  local outputString = outputStr or ''
+  local methodNode = graph:findNodeByID( methodNodeID )
+  
+  if (methodNode == nil) then
+    return nil
+  end
+
+  local classCalledWithin = classCalledFrom
+  if (methodNode.meta.type == 'Method') then
+    classCalledWithin = graph:findEdgesByTarget( methodNodeID, 'Contains' )[1].from[1].data.name
+    outputString = outputString .. "activate " .. classCalledWithin .. "\n"
+  end
+
+  local methodExecutions = graph:findEdgesBySource( methodNodeID, 'Executes' )
+
+  for index, edge in pairs(methodExecutions) do
+    local actualNode = edge.to[1]
+
+    print("CMON... LOG!", actualNode.data.name)
+    print(outputString)
+
+    if (actualNode.meta.type == 'Method') then
+      
+      local classCalledTo = graph:findEdgesByTarget( actualNode.id, 'Contains' )[1].from[1].data.name
+
+      outputString = outputString .. classCalledWithin .. " -> " .. classCalledTo .. " : " .. actualNode.data.name .. "\n"
+      -- outputString = outputString .. "activate " .. classCalledTo .. "\n"
+
+      -- recursive call
+      outputString = filterSequenceDiagram( graph, actualNode.id, outputString )
+
+      -- outputString = outputString .. "deactivate " .. classCalledTo .. "\n"
+
+    elseif (actualNode.meta.type == 'Condition') then
+
+      local conditionBranches = graph:findEdgesBySource( actualNode.id, 'HasBranch' )
+
+      for key, branch in pairs(conditionBranches) do
+
+        local branchNode = branch.to[1]
+
+        if (key == 1) then
+          outputString = outputString .. "alt " .. branchNode.data.name .. "\n"
+          outputString = filterSequenceDiagram( graph, branchNode.id, outputString, classCalledWithin )
+        else
+          outputString = outputString .. "else " .. branchNode.data.name .. "\n"
+          outputString = filterSequenceDiagram( graph, branchNode.id, outputString, classCalledWithin )
+        end
+
+      end
+
+      outputString = outputString .. "end\n"
+    elseif (actualNode.meta.type == 'Loop') then
+
+      local loopHeaderNode = graph:findEdgesBySource( actualNode.id, 'HasHeader' )[1].to[1]
+      outputString = outputString .. "loop " .. loopHeaderNode.data.name .. "\n"
+
+      outputString = filterSequenceDiagram( graph, actualNode.id, outputString, classCalledWithin )
+      outputString = outputString .. "end\n"
+    end
+  end
+
+  if (methodNode.meta.type == 'Method') then
+    outputString = outputString .. "deactivate " .. classCalledWithin .. "\n"
+  end
+
+  return outputString
+end
+
+-- Takes string and generates svg image for sequence diagram
+local function generateSVG( sequenceDiagramString )
+
+end
+
+local function getImg(graph, methodID)
+  local outputString = ''
+  outputString = outputString .. "@startuml\n"
+
+  outputString = filterSequenceDiagram( graph, methodID, outputString )
+
+  outputString = outputString .. "@enduml"
+
+  local file = io.open("plantUml.txt", "w")
+  file:write(outputString)
+  file:close()
+
+  generateSequenceDiagramImage()
+end
+
+-----------------------------------------------------
+
+
+
+
 return {
-  generateSequenceDiagramTxt = generateSequenceDiagramTxt,
-  generateSequenceDiagramImage = generateSequenceDiagramImage
+  getImg = getImg
 }
